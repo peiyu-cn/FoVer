6d6e05c36c370b306a44566b8a40840925f9c3bb
PeiYu <peiyu@nlpg.bjut>
2024-09-02 14:59:13 +0800

    update demos

--- demos.py
+++ demos.py
@@ -83,8 +83,75 @@ def williamhowardtaft_was_president_of_us_when_superconductivity_was_discovered(
 	l.claims = [
 		(happen_in(discover_superconductivity) == 1911, "Superconductivity was discovered in 1911."),
 		(
-			ForAll([i], (president_of_when(williamhowardtaft, unitedstates, i)) == And(i >= 1909, i <= 1913)),
+			ForAll([i], Implies(And(i >= 1909, i <= 1913), (president_of_when(williamhowardtaft, unitedstates, i)))),
 			"William Howard Taft was president from 1909 to 1913.",
+		),
+	]
+	#  Unstated assumptions that are necessary for the reasoning in the text, regardless of whether they are true or not.
+	l.assumptions = [
+		(us == unitedstates, "U.S. is United States."),
+	]
+
+	return l
+
+# %% demo 1 candidate
+## User:
+"""Question: Who was president of the U.S. when superconductivity was discovered?
+Answer:
+Superconductivity was discovered in 1911 by Heike Kamerlingh Onnes. Woodrow Wilson was president of the United States from 1913 to 1921. So the final answer (the name of the president) is: Woodrow Wilson."""
+## Assistant:
+def woodrowwilson_was_president_of_us_when_superconductivity_was_discovered(**kwargs):
+	"""
+	Superconductivity was discovered in 1911 by Heike Kamerlingh Onnes.
+	Woodrow Wilson was president of the United States from 1913 to 1921.
+	
+	Target: Woodrow Wilson was president of the U.S. when superconductivity was discovered.
+	Answer: Woodrow Wilson.
+	"""
+	# Initialize an instance of QALogic with given arguments.
+	l = QALogic(**kwargs)
+
+	# Define types.
+	Name = DeclareSort('Name')
+	Event = DeclareSort('Event')
+	Region = DeclareSort('Region')
+	# Define functions.
+	president_of_when = Function('is-president-of-when', Name, Region, IntSort(), BoolSort()) # (Name, Region, Int) -> Bool Name is president of Region when Int. # Using int as input and bool as output because this is not *-to-1 relation.
+	happen_in = Function('happen-in', Event, IntSort()) # (Event) -> Int, Event happen in Int. # Using int as output because event time is *-to-1 relation.
+	#  Target function:
+	president_of_when_event = Function('is-president-of-when-event', Name, Region, Event, BoolSort()) # (Name, Region, Event) -> Bool, Name is president of Region when Event.
+
+	# Arrange instances.
+	discover_superconductivity = Const('discover-superconductivity', Event)
+	woodrowwilson = Const('Woodrow Wilson', Name)
+	us = Const('U.S.', Region)
+	unitedstates = Const('United States', Region)
+
+	# Target.
+	l.answer = woodrowwilson
+	l.answer_type = Name
+	l.question = lambda n: president_of_when_event(n, us, discover_superconductivity)
+
+	# Implementations.
+	#  Local placeholders that will be used by quantifiers.
+	i = Int('i')
+	n = Const('n', Name)
+	e = Const('e', Event)
+	r = Const('r', Region)
+
+	#  Relation Definitions
+	l.definations = [
+		(
+			ForAll([n, e, r], president_of_when_event(n, r, e) == Exists([i], And(happen_in(e) == i, president_of_when(n, r, i)))),
+			"A person was president when an event happened if and only if the event happened in the year that person was president."
+		),
+	]
+	#  Claims from text
+	l.claims = [
+		(happen_in(discover_superconductivity) == 1911, "Superconductivity was discovered in 1911."),
+		(
+			ForAll([i], Implies(And(i >= 1913, i <= 1921), (president_of_when(woodrowwilson, unitedstates, i)))),
+			"Woodrow Wilson was president from 1913 to 1921.",
 		),
 	]
 	#  Unstated assumptions that are necessary for the reasoning in the text, regardless of whether they are true or not.
